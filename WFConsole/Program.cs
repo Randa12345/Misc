a5a7f673abd37dc736b0d3945270ab3c685a5953
unknown <randaa@SSTA-DEV4.AutoTagAmerica.com>
2015-09-04 11:37:36 -0400

    'First1'

---
+++ WFConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Activities;
+using System.Activities.Statements;
+
+namespace WFConsole
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Activity wfHello = new Hello();
+            WorkflowInvoker.Invoke(wfHello);
+
+            Console.ReadLine();
+        }
+    }
+}
